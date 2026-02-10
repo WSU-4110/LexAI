f238d9847186dd32a8e916c334e07b812be4749f
Hassan Alkhafaji <alkhafajihassan@gmail.com>
2026-02-10 11:44:55 -0500

    project setup

---
+++ LexAI_iOS/LexAI_iOS/LexAI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexAI_iOSApp.swift
+//  LexAI_iOS
+//
+//  Created by Hassan Alkhafaji on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LexAI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
