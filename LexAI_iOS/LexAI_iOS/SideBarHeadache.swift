d4fcdf076fa826830934fe714c5adc07a547b084
Hassan Alkhafaji <alkhafajihassan@gmail.com>
2026-04-07 22:29:27 -0400

    Trying out some changes

---
+++ LexAI_iOS/LexAI_iOS/SideBarHeadache.swift
@@ -0,0 +1,7 @@
+//
+//  SideBarHeadache.swift
+//  LexAI_iOSTests
+//
+//  Created by Sara Al-hachami on 4/1/26.
+//  Made for Unit Testing - CSC 4110 Assignment 5
+//
--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,6 @@ echo "Starting build and test..."
 xcodebuild clean test \
   -project LexAI_iOS/LexAI_iOS.xcodeproj \
   -scheme LexAI_iOS \
-  -destination 'platform=iOS Simulator,name=iPhone 16,OS=18.5'
+  -destination 'platform=iOS Simulator,name=iPhone 17 Pro,OS=26.2'
 
 echo "Build and tests completed successfully!"
